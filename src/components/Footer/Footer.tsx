ad482e37dd45c935e774beb0c59cb7fa5d51167c
Harsh Patel <hp92448@gmail.com>
2025-12-08 20:33:29 +0530

    error 1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
+interface FooterProps {
+  className?: string;
+}
 
-const Footer = () => (
-  <footer className="bg-[#002e45] text-white py-4 text-center text-sm">
+const Footer = ({ className = '' }: FooterProps) => (
+  <footer className={`bg-[#002e45] text-white py-4 text-center text-sm ${className}`}>
     &copy; {new Date().getFullYear()} Wealthyvia. All rights reserved.
   </footer>
 );

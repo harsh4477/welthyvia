2e03fa59b8b598f43daacbf7f8b02242127ad862
Harsh Patel <hp92448@gmail.com>
2025-12-08 20:15:23 +0530

    remove footer

--- src/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-// const Footer = () => (
-//   <footer className="bg-[#002e45] text-white py-4 text-center text-sm">
-//     &copy; {new Date().getFullYear()} Wealthyvia. All rights reserved.
-//   </footer>
-// );
-
-// export default Footer;

fe3e06ac8efb63ecb4800eba1cf667767d6a6ceb
Harsh Patel <hp92448@gmail.com>
2025-12-08 20:25:50 +0530

    error solve

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+
+const Footer = () => (
+  <footer className="bg-[#002e45] text-white py-4 text-center text-sm">
+    &copy; {new Date().getFullYear()} Wealthyvia. All rights reserved.
+  </footer>
+);
+
+export default Footer;
